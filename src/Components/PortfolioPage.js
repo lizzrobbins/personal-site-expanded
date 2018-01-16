b9d698d967d11eb5a8987cc45c9ac38159dda72b
Elizabeth Robbins <EROBBINS1479@GMAIL.COM>
2018-01-16 14:58:44 -0700

    formatting the portfolio items

--- src/Components/PortfolioPage.js
+++ src/Components/PortfolioPage.js
@@ -5,8 +5,8 @@ class PortfolioPage extends Component {
     return(
       <div className="portfolio-container">
         <p className="portfolio-item">
-          <p className="project-title">Roomie Owes How Much?</p>
-          <p className="project-links">erobbins1479.surge.sh | github.com/lizzrobbins/q1-project</p>
+          <h3 className="project-title">Roomie Owes How Much?</h3>
+          <p className="project-links"><a href="erobbins1479.surge.sh">erobbins1479.surge.sh</a> | <a href="github.com/lizzrobbins/q1-project">github.com/lizzrobbins/q1-project</a></p>
 
           <p className="project-description">•	A roommate bill calculator that allows for an easy and streamlined way to calculate the rent totals for multiple roommates</p>
 
