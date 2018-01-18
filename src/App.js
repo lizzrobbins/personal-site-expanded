378ff8f27991a8f6732e4c91305b994af296e8d5
Elizabeth Robbins <EROBBINS1479@GMAIL.COM>
2018-01-18 15:41:23 -0700

    formatted the contact-icons and added functional links

--- src/App.js
+++ src/App.js
@@ -5,51 +5,9 @@ import Nav from './Components/Nav.js';
 import MainTitle from './Components/MainTitle.js';
 import AboutSection from './Components/AboutSection.js';
 import PortfolioPage from './Components/PortfolioPage.js';
-import ContactMe from './Components/ContactMe.js';
 import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      main: true,
-      about: false,
-      portfolio: false
-    }
-  }
-
-  toggleMain = (e) => {
-    e.preventDefault()
-    if (this.state.main === false) {
-      this.setState({main: true})
-    }
-    this.setState({
-      about: false,
-      portfolio: false
-    })
-  }
-
-  toggleAbout = (e) => {
-    e.preventDefault()
-    if (this.state.about === false) {
-      this.setState({about: true})
-    }
-    this.setState({
-      main: false,
-      portfolio: false
-    })
-  }
-
-  togglePortfolio = (e) => {
-    e.preventDefault()
-    if (this.state.portfolio === false) {
-      this.setState({portfolio: true})
-    }
-    this.setState({
-      main: false,
-      about: false
-    })
-  }
 
   render() {
     return (
@@ -57,16 +15,12 @@ class App extends Component {
         <header className="App-header">
           <img src={purpleLogo} className="App-logo-purple" alt="purple logo" />
           <img src={logo} className="App-logo" alt="logo" />
-          <Nav
-            toggleMain={this.toggleMain}
-            toggleAbout={this.toggleAbout}
-            togglePortfolio={this.togglePortfolio}/>
+          <Nav />
           <MainTitle />
         </header>
         <main>
           <AboutSection />
           <PortfolioPage />
-          <ContactMe />
         </main>
       </div>
     );
--- src/Components/MainTitle.js
+++ src/Components/MainTitle.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import emailIcon from '../email-icon.png';
+import linkedinIcon from '../linkedin-icon.png';
+import githubIcon from '../github-icon.png';
 
 class MainTitle extends Component {
   render() {
@@ -10,6 +13,11 @@ class MainTitle extends Component {
           <p className="my-handle">
             Full Stack Developer | UX Enthusiast
           </p>
+          <div className="contact-icons">
+            <a href="mailto:me@lizzrobbins.com" target="_blank"><img src={emailIcon} className="email-icon" alt="email icon" /></a>
+            <a href="https://www.linkedin.com/in/lizzrobbins/" target="_blank"><img src={linkedinIcon} className="linkedin-icon" alt="LinkedIn icon" /></a>
+            <a href="https://github.com/lizzrobbins" target="_blank"><img src={githubIcon} className="github-icon" alt="GitHub icon" /></a>
+          </div>
         </div>
     )
   }
