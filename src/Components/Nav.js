a47173d1c448155e510fbeca7184e135d5906fab
Elizabeth Robbins <EROBBINS1479@GMAIL.COM>
2018-02-05 11:11:47 -0700

    added link for resume

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -7,7 +7,7 @@ class Nav extends Component {
         <a onClick={this.props.toggleMain} className="nav-links">HOME</a>
         <a href="#about" className="nav-links">ABOUT</a>
         <a href="#portfolio" className="nav-links">PORTFOLIO</a>
-        <a className="nav-links">RESUME</a>
+        <a href="https://drive.google.com/file/d/1x5chLA0mDRFHjBzTJuTPsk0j4LKYmo0q/view?usp=sharing" target="_blank" rel="noopener noreferrer" className="nav-links">RESUME</a>
       </nav>
     )
   }
