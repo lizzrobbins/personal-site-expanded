fca17dddbc7ddcc812feaf414e55aaae4b459643
Elizabeth Robbins <EROBBINS1479@GMAIL.COM>
2018-02-06 08:52:21 -0700

    working on nav hover transformation

--- src/Components/MainTitle.js
+++ src/Components/MainTitle.js
@@ -14,9 +14,11 @@ class MainTitle extends Component {
             Full Stack Developer | UX Enthusiast
           </p>
           <div className="contact-icons">
-            <a href="mailto:me@lizzrobbins.com" target="_blank" rel="noopener noreferrer"><img src={emailIcon} className="email-icon" alt="email icon" /></a>
-            <a href="https://www.linkedin.com/in/lizzrobbins/" target="_blank" rel="noopener noreferrer"><img src={linkedinIcon} className="linkedin-icon" alt="LinkedIn icon" /></a>
-            <a href="https://github.com/lizzrobbins" target="_blank" rel="noopener noreferrer"><img src={githubIcon} className="github-icon" alt="GitHub icon" /></a>
+            <div className="contact-container">
+              <a href="mailto:me@lizzrobbins.com" target="_blank" rel="noopener noreferrer"><img src={emailIcon} className="email-icon" alt="email icon" /></a>
+              <a href="https://www.linkedin.com/in/lizzrobbins/" target="_blank" rel="noopener noreferrer"><img src={linkedinIcon} className="linkedin-icon" alt="LinkedIn icon" /></a>
+              <a href="https://github.com/lizzrobbins" target="_blank" rel="noopener noreferrer"><img src={githubIcon} className="github-icon" alt="GitHub icon" /></a>
+            </div>
           </div>
         </div>
     )
